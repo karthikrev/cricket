5ff93d3427b2dcee4a306786fe87782c6808ecfd
karthikrev <kumarkarthikn@gmail.com>
2013-01-11 18:31:01 +0530

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,78 +1,105 @@
-const Mainloop = imports.mainloop;
-const PanelMenu=imports.ui.panelMenu;
-const St=imports.gi.St;
-const Main=imports.ui.main;
-const Shell=imports.gi.Shell;
-const Lang=imports.lang;
-const PopupMenu=imports.ui.popupMenu;
+const St = imports.gi.St;
+const Lang = imports.lang;
+const PanelMenu = imports.ui.panelMenu;
+const PopupMenu = imports.ui.popupMenu;
+const Main = imports.ui.main;
 const GLib = imports.gi.GLib;
+const Util = imports.misc.util;
+const Mainloop = imports.mainloop;
 
 const Soup = imports.gi.Soup;
 const _httpSession = new Soup.SessionAsync();
 Soup.Session.prototype.add_feature.call(_httpSession, new Soup.ProxyResolverDefault());
 
-let _uptime_indicator_object=null;
+let	url='http://www.espncricinfo.com/india-v-england-2012/engine/current/match/565812.html';
 
-function _getIndiaMatch(unxml_rss){
-		unxml_rss=unxml_rss.replace(/^<\?xml\s+version\s*=\s*(["'])[^\1]+\1[^?]*\?>/, "");
-		try{
-        	var rss = new XML(unxml_rss);
-			global.log("Length: " +rss.channel.item.length());
-        	for (var i = 0; i < rss.channel.item.length(); i++) {
-				var c_match=rss.channel.item[i].title.toString();
-				global.log("Match:"+c_match);
-				if(c_match.match(/India\s+[0-9]+\/[0-9]+/)) {
-					c_match = c_match.replace(/<[^>]*>/,"").replace(/<[^>]*>/,"");
- 					return c_match;	
-				}
-        	}
-			return "No India Match";
-		}
-		catch(err){
-			global.log('ERROR:' +err);
-		}
+function CpuFreq() {
+    this._init.apply(this, arguments);
 }
 
+CpuFreq.prototype = {
+	__proto__: PanelMenu.SystemStatusButton.prototype,
 
-const CricketUpdator=new Lang.Class({
-	Name: 'CricketUpdator',
-	_init: function() {
+	_init: function(){	
+		PanelMenu.SystemStatusButton.prototype._init.call(this, 'cric');
+		this.statusLabel = new St.Label({text: '--'});
+		this._build_ui();
 		event = GLib.timeout_add_seconds(0, 5, Lang.bind(this, function () {
-			this._display();
+			//this._get_price();
+			this._update_gui();
+//			this._build_ui();
+			return true;	
 		}));
-   	},
+	},
 
-	_display: function(){
-		this._getRSS('placeholder', function(response_rss){
-			display_text=_getIndiaMatch(response_rss);
-			this.actor = new St.Button();
-        	this._label = new St.Label({ text: display_text });
-        	this.actor.set_child(this._label);
-        	Main.panel._centerBox.add(this.actor, { y_fill: true });
+
+	_update_gui: function(){
+		this._wget(url,function(data){
+			this.data=data;
+			try{
+				cric_data=data.split('</title>')[0].split('<title>')[1].split('|')[0];
+				score=cric_data.split('(')[0];
+                this.actor.get_children().forEach(function(c) {  c.destroy() });
+				this.statusLabel = new St.Label({text: '--'});
+				this.statusLabel.set_text(score);
+				this.actor.add_actor(this.statusLabel);
+				this.menu.removeAll();
+        		perdetails=cric_data.split('(')[1].split(')')[0].split(',')
+        		overs=perdetails[0];
+        		Batsman=perdetails[1];
+        		Runner=perdetails[2];
+        		Bowler=perdetails[3];
+				//this.menu.box.get_children().forEach(function(c) { c.destroy() });
+				let detailed_scores = new PopupMenu.PopupMenuSection("Cricket");
+				detailed_scores.addMenuItem(this._createMenuItem("Overs: " +overs));
+				detailed_scores.addMenuItem(this._createMenuItem("Batting: " +Batsman + ' & ' + Runner));
+				detailed_scores.addMenuItem(this._createMenuItem("Bowler: " + Bowler));
+				this.menu.addMenuItem(detailed_scores);
+			}
+			catch(err){
+				global.log("Error: cant update" +err);
+			}
 		});
 	},
 
- 	_getRSS: function(url, callback) {
-    	var request = Soup.Message.new('GET', 'http://rss.cricinfo.com/rss/livescores.xml');
-        _httpSession.queue_message(request, function(_httpSession, message) {
-        	if (message.status_code !== 200) {
-        		callback(message.status_code, null);
-        		return;
-        	}
-        	full_rss = request.response_body.data;
-        	callback.call(null, full_rss);
+    _createMenuItem: function(data){
+        menu_item = new PopupMenu.PopupMenuItem("");
+        menu_item.addActor(new St.Label({ text: data}));
+		// item.connect('activate',function() {  Util.spawn(command);   });	 //TODO
+        return menu_item;		
+	},
+
+    _wget: function(url, callback) {
+                here = this;
+                let session = new Soup.SessionAsync();
+                let message = Soup.Message.new('GET', url);
+                session.queue_message(message, function(session, message) {
+                    response = message.response_body.data;
+                    callback.call(here, response);
+                });
+        },
+	
+	_build_ui: function() {
+		this.actor.get_children().forEach(function(c) {
+			 c.destroy()
 		});
+		this._update_gui();
 	}
-});
-
-function enable(){
-   try {
-      new CricketUpdator;
-   }
-   catch(err) {
-      global.log("Error enabling Uptime Indicator extension: "+err.message);
-   }
 }
-function disable(){}
-function init(){}
 
+function init() {
+}
+
+let indicator;
+let event=null;
+
+function enable() {
+    indicator = new CpuFreq();
+    Main.panel.addToStatusArea('cric', indicator);
+}
+
+function disable() {
+    indicator.destroy();
+    Mainloop.source_remove(event);
+    indicator = null;
+}
